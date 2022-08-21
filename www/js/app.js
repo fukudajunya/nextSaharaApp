01f22114101d2bbf4c8ee16c73f05555a10a888f
fukudajunya <f.econ.j.1019@gmail.com>
2022-08-21 18:03:55 +0900

    Project's Initial Commit

---
+++ www/js/app.js
@@ -0,0 +1,5 @@
+const applicationKey = 'ea4817d0235beacbf9d50b90c14a6be4c9d3845960f7283efd002f1969ef2d47';
+const clientKey = '73510f0916ac4aa2120a181dfc0e5fe0bfa7d2ac1003c2cf51dad94e4770e8db';
+
+const ncmb = new NCMB(applicationKey, clientKey);
+window.task = null;
